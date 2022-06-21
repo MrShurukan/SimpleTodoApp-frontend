dda70e90e8252bd1ecbc716972e8b3049f9457e3
Ilya Zavyalov <ilya.zavyalov0@gmail.com>
2022-06-21 12:32:18 +0300

    WIP с ноутбука

--- js/index.js
+++ js/index.js
@@ -3,7 +3,14 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            tabs: [{name: "Покупки", isActive: false }, {name: "Фигма", isActive: true }] 
+            tabs: [
+                {id: 1, name: "Покупки", isActive: true, todoItems: 
+                    [{id: 1, text: "Это test todo!", isChecked: true}, 
+                    {id: 2, text: "Это второй test todo!", isChecked: true}, 
+                    {id: 3, text: "Третий todo!", isChecked: false}] }, 
+                    
+                {id: 2, name: "Фигма", isActive: false }, 
+                {id: 3, name: "Ещё одна вкладка", isActive: false}] 
         }
     }
 }).mount('#app')
